c8048db4c78efe164382658302143497ad425857
María Paula Calderón <mpc441@nyu.edu>
2020-04-21 20:51:48 +0400

    made changes to port

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public')); //or: '../../build'
 
